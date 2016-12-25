893ea6b5a665c4117e14dac625286516a56abab8
Kasem Arnontavilas <mrtomyum@gmail.com>
2016-12-26 00:20:58 +0700

    Update index.html add bootstrap

--- main.go
+++ main.go
@@ -28,10 +28,6 @@ func main() {
 func router(r *gin.Engine) *gin.Engine{
 	//r.LoadHTMLGlob("view/**/*.html")
 	//r.Static("/", "./view/html")
-	//r.Static("/js", "./view/public/js")
-	//r.Static("/css", "./view/public/css")
-	//r.Static("/img", "./view/public/img")
-	//r.Static("/json", "./view/public/json")
 	r.Use(static.Serve("/", static.LocalFile("view", true)))
 	r.GET("/ws", func(c *gin.Context){
 		wsServ(c.Writer, c.Request)
