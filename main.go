c698cb3e2680fa181990b9d3d1a767c86a1acf1a
Kasem Arnontavilas <mrtomyum@gmail.com>
2016-12-25 22:43:26 +0700

    แก้บัก เพิ่ม Cert TLS

--- main.go
+++ main.go
@@ -3,6 +3,16 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/contrib/static"
+	"net/http"
+	"github.com/gorilla/websocket"
+	"github.com/mrtomyum/mobile1/model"
+)
+
+var (
+	upgrader = websocket.Upgrader{
+		ReadBufferSize: 1024,
+		WriteBufferSize: 1024,
+	}
 )
 
 func main() {
@@ -23,5 +33,25 @@ func router(r *gin.Engine) *gin.Engine{
 	//r.Static("/img", "./view/public/img")
 	//r.Static("/json", "./view/public/json")
 	r.Use(static.Serve("/", static.LocalFile("view", true)))
+	r.GET("/ws", func(c *gin.Context){
+		wsServ(c.Writer, c.Request)
+	})
 	return r
+}
+
+func wsServ(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+	defer conn.Close()
+	clientName := r.Header.Get("Name")
+	c := model.Client{
+		Conn: conn,
+		Send: make(chan model.Message),
+		Name: clientName,
+	}
+	go c.Read()
+	go c.Write()
 }
